4aabc96e729cff075c606b4086578f9e9b928a90
ooxi <violetland@mail.ru>
2013-05-11 17:44:38 +0200

    Initial development
    Squashed commit of the following:
    
    commit a5d2addba90a62a2fc1a03bba2e41629e0f7a4cc
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:44:22 2013 +0200
    
        Hotfix
    
    commit 7f820ca59ea4678e8ffb980e80b1a50060e819db
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:42:40 2013 +0200
    
        Initial development
    
    commit bf46c7422ef0659359b627da09fa36d8352ceea4
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:30:41 2013 +0200
    
        jQuery 2.0.0
    
    commit 9e286d0a7f7a90a3cf7b770e69e31e89d7f4d1b5
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:29:22 2013 +0200
    
        Ignore temporary files
    
    commit 9809349f5e2c6e18e21f14f446cb36497f9b5724
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:29:11 2013 +0200
    
        Development...
    
    commit d260ae60886b8e1d11823001646958be908d1600
    Author: ooxi <violetland@mail.ru>
    Date:   Sat May 11 17:28:48 2013 +0200
    
        jQuery Cross Domain Support

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,35 @@
-console.log('This would be the main JS file.');
+$(function() {
+	$.get('http://www.di.fm/', function(content) {
+		var difm = $(content.responseText);
+
+		var channels = [];
+		var html = '';
+
+		difm.find('.lists a').each(function() {
+			var $this = $(this);
+			channels.push({
+				name:	$this.children('span').text(),
+				img:	$this.children('img').attr('src'),
+				url:	$this.attr('href')
+			});
+		});
+
+		html += '<ul class="channels">';
+		for (var i = 0; i < channels.length; ++i) {
+			var channel = channels[i];
+
+			html += ''
+				+'<li>'
+					+'<a href="http://listen.di.fm/public2'+ channel.url +'.pls">'
+						+'<img src="http:'+ channel.img +'" />'
+						+'<span>'+ channel.name +'</span>'
+					+'</a>'
+				+'</li>'
+			;
+		}
+		html += '</ul>';
+
+		$('#main_content').html(html);
+	});
+});
+
